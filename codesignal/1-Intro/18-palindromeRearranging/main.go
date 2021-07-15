ca3482f03974532fb8e9cc499690b01770e47332
davidmoca97 <davidmoca97@gmail.com>
2021-07-15 16:16:00 -0500

    18-palindromeRearranging

---
+++ codesignal/1-Intro/18-palindromeRearranging/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func palindromeRearranging(inputString string) bool {
+    if len(inputString) <= 2 {
+        return true
+    }
+    counter := make(map[rune]int)
+    for _, char := range inputString {
+        counter[char]++
+    }
+    var oddKeys int
+    for _, count := range counter {
+        if count %2 == 0{
+            continue
+        }
+        oddKeys++
+        if oddKeys > 1 {
+            return false
+        }
+    }
+    return true
+}
+
+// Challenge link
+// https://app.codesignal.com/arcade/intro/level-4/Xfeo7r9SBSpo3Wico
+func main() {
+	fmt.Println(palindromeRearranging("aabb")) //Should print true
+}
