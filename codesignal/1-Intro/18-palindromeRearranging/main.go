2f5a624e1e98b2b898d62f0ae60c959982097e07
davidmoca97 <davidmoca97@gmail.com>
2021-09-09 19:43:55 -0500

    fix 18

--- codesignal/1-Intro/18-palindromeRearranging/main.go
+++ codesignal/1-Intro/18-palindromeRearranging/main.go
@@ -3,24 +3,21 @@ package main
 import "fmt"
 
 func palindromeRearranging(inputString string) bool {
-    if len(inputString) <= 2 {
-        return true
-    }
-    counter := make(map[rune]int)
-    for _, char := range inputString {
-        counter[char]++
-    }
-    var oddKeys int
-    for _, count := range counter {
-        if count %2 == 0{
-            continue
-        }
-        oddKeys++
-        if oddKeys > 1 {
-            return false
-        }
-    }
-    return true
+	counter := make(map[rune]int)
+	for _, char := range inputString {
+		counter[char]++
+	}
+	var oddKeys int
+	for _, count := range counter {
+		if count%2 == 0 {
+			continue
+		}
+		oddKeys++
+		if oddKeys > 1 {
+			return false
+		}
+	}
+	return true
 }
 
 // Challenge link
