2c2c589428f8d0a60837453044b6c91a2dd20be9
davidmoca97 <davidmoca97@gmail.com>
2021-10-26 21:42:28 -0500

    breakingRecords

---
+++ hackerRank/algorithms/implementation/15-BreakingBestAndWorst/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+func breakingRecords(scores []int32) []int32 {
+	max, min := scores[0], scores[0]
+	var brokeMaxCount, brokeMinCount int32
+	for i := 1; i < len(scores); i++ {
+		if scores[i] > max {
+			max = scores[i]
+			brokeMaxCount++
+			continue
+		}
+		if scores[i] < min {
+			min = scores[i]
+			brokeMinCount++
+		}
+	}
+	return []int32{brokeMaxCount, brokeMinCount}
+}
+
+// Challenge:
+// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+func main() {
+	fmt.Println(breakingRecords([]int32{10, 5, 20, 20, 4, 5, 2, 25, 1}))
+}
